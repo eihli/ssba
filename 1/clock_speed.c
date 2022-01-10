51f2beb29255f01d3ad0eecaeb28f822066711c6
Eric Ihli <eihli@owoga.com>
2022-01-09 21:07:05 -0600

    Comment pangram ASM

--- 1/clock_speed.c
+++ 1/clock_speed.c
@@ -2,12 +2,16 @@
 #include <time.h>
 
 int main() {
-    clock_t start = clock(), diff;
-    unsigned long i = 0;
-    while (i < 4e9) {
+    clock_t start = clock(), elapsed;
+    unsigned long i, iters;
+    i = 0;
+    iters = 1e9;
+    while (i < iters) {
         i++;
     }
-    diff = clock() - start;
-    printf("CLOCKS_PER_SECOND: %lu\n", CLOCKS_PER_SEC);
-    printf("Processing took %f ms\n", 1.0 * diff * 1000 / CLOCKS_PER_SEC);
+    elapsed = clock() - start;
+    float elapsed_s = (float) elapsed / CLOCKS_PER_SEC;
+    float hz = iters / elapsed_s;
+    printf("Processing took\t%f seconds\n", elapsed_s);
+    printf("Estimated clock speed is\t%f GHz\n", hz / 1e9);
 }
--- 3/pangram.asm
+++ 3/pangram.asm
@@ -2,18 +2,26 @@
 
 global pangram
 pangram:
+	;; Use bits to keep track of which letters we've seen.
+	;; 'A' is character 65 in ASCII
+	;; 'z' is character 122 in ASCII
+	;; A 57 bit number can keep track of every character.
+	;;
+	;; Initialize a counter in RCX to loop through the string
+	;; and RAX to the bits to track which characters we've seen.
 	xor rcx, rcx
 	mov rax, (1 << 58) - 1
 loop:
 	cmp byte [rdi + rcx], 0
 	je end
-	;; Get current character.
-	;; Subtract 0x61
-	;; Shift left by rank
 	movzx r8, byte [rdi + rcx]
+	;; Whatever character we see, subtract 'A' from it so
+	;; that 'A' is 0, 'B', is 1, ..., and 'z' is 57.
 	sub r8, 65
 	mov r9, 1
 iter:
+	;; Position the bit representing the character.
+	;; Then zero out that bit in RAX.
 	cmp r8, 0
 	jle done
 	shl r9, 1
@@ -25,11 +33,17 @@ done:
 	inc rcx
 	jmp loop
 end:
-	;; Save off rax temporarily so we can shift it right for lowercase
+	;; Save off RAX to R10 temporarily so we can shift it right for lowercase
+	;; Now we've got occurrences of 'A' through 'Z' in RAX
+	;; and occurrences of 'a' through' 'z' in R10.
+	;;
+	;; If there's still a 1 in both the lowercase AND the uppercase bits,
+	;; then we haven't seen either, so this function should return FALSE.
 	mov r10, rax
 	shr r10, 32
 	and rax, r10
-	;; Zero out everything above 26th bit
+	;; Zero out everything above 26th bit. This makes it so we ignore the characters
+	;; between 'Z' and 'a'.
 	and rax, (1 << 27) - 1
 	cmp rax, 0
 	je true
