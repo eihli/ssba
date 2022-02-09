6e9cb4ca4efd2e71ed0134a719a58e7a68248944
Eric Ihli <eihli@owoga.com>
2022-02-09 09:08:14 -0600

    Finish header parse and start on packet

--- 10/parse_cap.c
+++ 10/parse_cap.c
@@ -9,6 +9,14 @@
 
 char buffer[BUF_LEN] = {0};
 
+typedef struct caph_t {
+    uint32_t magic_number;
+    uint16_t major_version;
+    uint16_t minor_version;
+    uint32_t snaplen;
+    uint32_t linktype;
+} caph_t;
+
 int main(int argc, char *argv[])
 {
     if (argc != 2) {
@@ -22,9 +30,18 @@ int main(int argc, char *argv[])
         fprintf(stderr, "open failed %m\n");
         return 1;
     }
-    ssize_t sz;
-    sz = read(fd, buffer, 4);
-    
-    printf("Magic Number: %X\n", ntohl(*((uint32_t *) buffer)));
+    caph_t header = {0};
+    read(fd, &header.magic_number, 4);
+    read(fd, &header.major_version, 2);
+    read(fd, &header.minor_version, 2);
+    // Skip 64 bits of "reserved" that MUST be ignored.
+    lseek(fd, 8, SEEK_CUR);
+    read(fd, &header.snaplen, 4);
+    read(fd, &header.linktype, 4);
+    printf("Magic Number: %X\n", header.magic_number);
+    printf("Major Version: %hX\n", header.major_version);
+    printf("Minor Version: %hX\n", header.minor_version);
+    printf("Snaplen: %d\n", header.snaplen);
+    printf("Linktype: %X\n", header.linktype);
     
 }
