fcbe3a3f4118c1bc947b3e53532b05e0cc4bd1f3
Eric Ihli <eihli@owoga.com>
2022-02-09 23:26:36 -0600

    Start parsing packets

--- 10/parse_cap.c
+++ 10/parse_cap.c
@@ -1,14 +1,23 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 // I don't know what the maximum length I might want to read
 // as part of parsing a cap file. But 2048 seems like a reasonable start.
+// This is the buffer for the data of a single packet.
 #define BUF_LEN 2048
+
+// We can't know how many packets are in a cap file until
+// we finish parsing it, so we'll have buffer of arbitrary size for that too.
+#define PACKETS_BUF_LEN 8192
 
 char buffer[BUF_LEN] = {0};
 
+// Captured packet header
 typedef struct caph_t {
     uint32_t magic_number;
     uint16_t major_version;
@@ -16,6 +25,17 @@ typedef struct caph_t {
     uint32_t snaplen;
     uint32_t linktype;
 } caph_t;
+// Packet record
+typedef struct packetrec_t {
+    uint32_t timestamp_seconds;
+    uint32_t timestamp_fractional;
+    uint32_t cap_len;
+    uint32_t orig_len;
+    uint8_t *data;
+} packetrec_t;
+
+int num_packets = 0;
+packetrec_t packets[PACKETS_BUF_LEN] = {0};
 
 int main(int argc, char *argv[])
 {
@@ -30,6 +50,9 @@ int main(int argc, char *argv[])
         fprintf(stderr, "open failed %m\n");
         return 1;
     }
+    
+    struct stat f_stat;
+    fstat(fd, &f_stat);
     caph_t header = {0};
     read(fd, &header.magic_number, 4);
     read(fd, &header.major_version, 2);
@@ -44,4 +67,16 @@ int main(int argc, char *argv[])
     printf("Snaplen: %d\n", header.snaplen);
     printf("Linktype: %X\n", header.linktype);
     
+
+    packetrec_t packet = {0};
+    for (int i = 0; i < PACKETS_BUF_LEN && lseek(fd, 0, SEEK_CUR) < f_stat.st_size; i++) {
+        read(fd, &packet.timestamp_seconds, 4);
+        read(fd, &packet.timestamp_fractional, 4);
+        read(fd, &packet.cap_len, 4);
+        read(fd, &packet.orig_len, 4);
+        packet.data = malloc(packet.cap_len * sizeof(uint8_t));
+        read(fd, packet.data, packet.cap_len);
+        memcpy(&packets[i], &packet, sizeof(packetrec_t));
+        printf("Packet: %d \t Timestamp: %d \t Len: %d \t Orig: %d\n", i, packet.timestamp_seconds, packet.cap_len, packet.orig_len);
+    }
 }
